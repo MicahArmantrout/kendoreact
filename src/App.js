a9f378649fe68cb9ace70d8cfc30e74269856cff
root <micaha@producealliance.com>
2018-01-23 14:49:07 -0600

    3rd

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DatePicker } from '@progress/kendo-dateinputs-react-wrapper';
+import '@progress/kendo-ui';
+import '@progress/kendo-theme-default/dist/all.css'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      dateTime: new Date()
+    };
+
+    //this.onChange - this.onChange.bind(this);
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
